76b9f0dcbdee1e42accdd84a45b2565ce6205f63
Leandro Cardoso <leandrocardosodev@gmail.com>
2026-05-04 01:42:13 -0300

    Temp MainMenuScene

---
+++ src/scene/MainMenuScene.cpp
@@ -0,0 +1,44 @@
+#include "MainMenuScene.hpp"
+#include "../input/InputManager.hpp"
+
+// Temp:
+extern InputManager input;
+
+MainMenuScene::MainMenuScene() {
+    options = {"Novo Jogo", "Opcoes", "Sair"};
+    selectedIndex = 0;
+}
+
+void MainMenuScene::update() {
+
+    if (input.isKeyPressed(SDL_SCANCODE_DOWN)) {
+        selectedIndex++;
+        if (selectedIndex >= options.size())
+            selectedIndex = 0;
+    }
+
+    if (input.isKeyPressed(SDL_SCANCODE_UP)) {
+        selectedIndex--;
+        if (selectedIndex < 0)
+            selectedIndex = options.size() - 1;
+    }
+}
+
+void MainMenuScene::render(Renderer& renderer) {
+
+    int startY = 200;
+
+    for (int i = 0; i < options.size(); i++) {
+
+        int x = 300;
+        int y = startY + i * 50;
+
+        // selecao:
+        if (i == selectedIndex) {
+            renderer.drawRect(x - 20, y - 10, 200, 40);
+        }
+
+        // simulando texto:
+        renderer.drawRect(x, y, 150, 30);
+    }
+}
---
+++ src/scene/MainMenuScene.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "Scene.hpp"
+
+class MainMenuScene : public Scene {
+private:
+    std::vector<std::string> options;
+    int selectedIndex;
+
+public:
+    MainMenuScene();
+
+    void update() override;
+    void render(Renderer& renderer) override;
+};
