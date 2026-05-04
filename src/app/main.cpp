8272a6e5a54f5b0a913cf700a662108062844712
Leandro Cardoso <leandrocardosodev@gmail.com>
2026-05-04 00:20:58 -0300

    Test test scene

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,12 +1,14 @@
 #include "../core/Core.hpp"
+#include "../scene/TestScene.hpp"
 
 // COMPILAR:
-// g++ src/app/main.cpp src/core/Core.cpp -o build/game.exe
+// g++ src/app/main.cpp src/core/Core.cpp src/scene/TestScene.cpp -o build/game.exe
 
 int main() {
     Core core;
 
     core.init();
+    core.setScene(new TestScene());
     core.run();
     core.shutdown();
 
